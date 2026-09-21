58266da046efa14591f1d3a8a6bec07466611385
EMPP0112 <EMPP0112@ACADIAN.LOCAL>
2026-09-21 17:04:23 +0530

    added vercel.json file

--- server/src/config/cors.js
+++ server/src/config/cors.js
@@ -1,7 +1,17 @@
 const config = require('./config');
 
+const allowedOrigins = config.clientUrl
+  ? config.clientUrl.split(',').map((url) => url.trim())
+  : ['http://localhost:5173'];
+
 const corsOptions = {
-  origin: config.clientUrl,
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
